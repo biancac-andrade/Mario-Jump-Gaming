f32614b0af1c71e3fbda1fd0603555b71b5ef1e1
Bianca Cristina <bc.andrade05@gmail.com>
2023-01-26 00:39:20 -0300

    Bianca: Cria condicional para inicio e fim jogo

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 //declaração variáveis por Diego Araújo
-
 var pipe, pipeImg;
 var cloud, cloudImg, cloud2, cloudImg2;
 var block, blockImg;
@@ -8,6 +7,7 @@ var flower, flowerImg;
 var floorInvisible;
 var mario, marioImg;
 var theEnd, theEndImg;
+var state = "go";
 
 // declaração de variaveis dos grupos por Bianca Cristina
 var grupoBarreiras, grupoClouds;
@@ -21,49 +21,85 @@ function preload() {
   floorImg = loadImage("img/floor.png");
   flowerImg = loadImage("img/flower.png");
   marioImg = loadAnimation("img/mario1.png", "img/mario2.png");
-  theEndImg = loadAnimation("img/theEnd.jpeg");
+  theEndImg = loadImage("img/theEnd2.png");
 }
 
 function setup() {
   createCanvas(1365, 627);
 
   // posição, tamanhos do mario por Bianca Cristina
-  mario = createSprite(100, 140, 20, 20);
+  mario = createSprite(100, 50, 20, 10);
   mario.addAnimation("mario", marioImg);
-  mario.scale = 0.5;
-  mario.x = 50;
+  mario.scale = 0.9;
+  mario.x = 100;
 
   // posição, tamanhos do chão por Bianca Cristina
-  floor = createSprite(100, 180, 160, 20);
+  floor = createSprite(100, 610, 20, 20);
   floor.addImage("floor", floorImg);
-  floorInvisible = createSprite(200, 170, 800, 10);
-  floorInvisible.visible = false;
+  floor.scale = 2;
+  floorInvisible = createSprite(50, 580, 160, 10);
+  floorInvisible.visible = true;
 
   // chamada de grupos por Bianca Cristina
   grupoBarreiras = new Group();
   grupoClouds = new Group();
+
+  // chamada de imagem de fim de jogo por Bianca Cristina
+  theEnd = createSprite(690, 240, 20, 20);
+  theEnd.addImage(theEndImg);
+  theEnd.scale = 0.8;
+  theEnd.visible = false;
 }
 
 function draw() {
   // cor do fundo por Bianca Cristina
   background("#BFF0D4");
 
-  // a velocidade e divisão do chão  por Bianca Cristina
-  floor.velocityX = -2;
-  if (floor.x < 0) {
-    floor.x = floor.width / 2;
-  }
+  // cria condicional para inicio e fim de jogo por Bianca Cristina
+  if (state === "go") {
+    // a velocidade e divisão do chão  por Bianca Cristina
+    floor.velocityX = -2;
+    if (floor.x < 0) {
+      floor.x = floor.width / 2;
+    }
 
-  // comando para pular, chão invisivel e gravidade do mario por Bianca Cristina
-  if (keyDown("space")) {
-    mario.velocityY = -10;
-  }
-  mario.velocityY = mario.velocityY + 0.8;
-  mario.collide(floorInvisible);
+    // comando para pular, chão invisivel e gravidade do mario por Bianca Cristina
+    if (keyDown("space")) {
+      mario.velocityY = -10;
+    }
+    mario.velocityY = mario.velocityY + 0.8;
+    mario.collide(floorInvisible);
 
-  // adicionando as barreiras e nuvens por Bianca Cristina
-  criaBarreira();
-  criaCloud();
+    // adicionando as barreiras e nuvens por Bianca Cristina
+    criaBarreira();
+    criaCloud();
+
+    if (grupoBarreiras.isTouching(mario)) {
+      state = "end";
+    }
+  } else if (state === "end") {
+    floor.velocityX = 0;
+    mario.velocityY = 0;
+
+    grupoBarreiras.setVelocityXEach(0);
+    grupoClouds.setVelocityXEach(0);
+
+    theEnd.visible = true;
+
+    textSize(28);
+
+    fill("#8B0000"); // cor da letra
+    text("Aperta o botão espaço para reiniciar o jogo!!!", 415, 50);
+
+    if (keyDown("space")) {
+      state = "go";
+      grupoBarreiras.destroyEach();
+      grupoClouds.destroyEach();
+
+      // nao ebibe a imagem de fim do jogo
+      theEnd.visible = false;
+    }
+  }
 
   drawSprites();
 }
@@ -71,7 +107,7 @@ function draw() {
 // função das barreias com posicao, velocidade, loop e tamanho e nomea o grupo de barreira  por Bianca Cristina
 function criaBarreira() {
   if (frameCount % 70 === 0) {
-    var barreira = createSprite(700, 150, 40, 10);
+    var barreira = createSprite(700, 548, 40, 10);
     barreira.velocityX = -2;
 
     var num = Math.round(random(1, 2, 3));
@@ -90,7 +126,7 @@ function criaBarreira() {
         break;
     }
 
-    barreira.scale = 0.05;
+    barreira.scale = 0.1;
 
     barreira.lifetime = 500;
     grupoBarreiras.add(barreira);
@@ -99,8 +135,8 @@ function criaBarreira() {
 
 // função das nuvens com posicao, velocidade, loop e tamanho e nomea o grupo de nuvens por Bianca Cristina
 function criaCloud() {
-  if (frameCount % 50 === 0) {
-    var cloud = createSprite(700, 50, 40, 10);
+  if (frameCount % 90 === 0) {
+    var cloud = createSprite(700, 300, 40, 10);
     cloud.velocityX = -2;
 
     var num = Math.round(random(1, 2));
@@ -116,7 +152,7 @@ function criaCloud() {
         break;
     }
 
-    cloud.scale = 0.1;
+    cloud.scale = 0.2;
     cloud.y - Math.round(random(15, 70));
     cloud.lifetime = 500;
 
